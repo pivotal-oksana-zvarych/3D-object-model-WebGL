cc719626988ae1c40ccad7098249bae670308113
xtreme-brandon-sproul <brandon@pivotallabs.com>
2014-09-19 15:15:52 -0400

    First commit

---
+++ WebGL-SampleWebApp/bin/Debug/AppX/js/diamond.js
@@ -0,0 +1,136 @@
+﻿var diamondVertexPositionBuffer;
+var diamondVertexColorBuffer;
+var diamondVertexIndexBuffer;
+
+function initBuffers() {
+    
+    diamondVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, diamondVertexPositionBuffer);
+    var vertices = [
+        // Front face
+        -1.0, -1.0, 1.0,
+         1.0, -1.0, 1.0,
+         1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0, 1.0, -1.0,
+         1.0, 1.0, -1.0,
+         1.0, -1.0, -1.0,
+
+        // Top face
+        -1.0, 1.0, -1.0,
+        -1.0, 1.0, 1.0,
+         1.0, 1.0, 1.0,
+         1.0, 1.0, -1.0,
+
+        // Bottom face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0, 1.0,
+        -1.0, -1.0, 1.0,
+
+        // Right face
+         1.0, -1.0, -1.0,
+         1.0, 1.0, -1.0,
+         1.0, 1.0, 1.0,
+         1.0, -1.0, 1.0,
+
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0, 1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, 1.0, -1.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    cubeVertexPositionBuffer.itemSize = 3;
+    cubeVertexPositionBuffer.numItems = 24;
+
+    diamondVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, diamondVertexColorBuffer);
+    var colors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [1.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 1.0, 0.0, 1.0], // Top face
+        [1.0, 0.5, 0.5, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 0.0, 1.0, 1.0]  // Left face
+    ];
+    var unpackedColors = [];
+    for (var i in colors) {
+        var color = colors[i];
+        for (var j = 0; j < 4; j++) {
+            unpackedColors = unpackedColors.concat(color);
+        }
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unpackedColors), gl.STATIC_DRAW);
+    cubeVertexColorBuffer.itemSize = 4;
+    cubeVertexColorBuffer.numItems = 24;
+
+    cubeVertexIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    var cubeVertexIndices = [
+        0, 1, 2, 0, 2, 3,    // Front face
+        4, 5, 6, 4, 6, 7,    // Back face
+        8, 9, 10, 8, 10, 11,  // Top face
+        12, 13, 14, 12, 14, 15, // Bottom face
+        16, 17, 18, 16, 18, 19, // Right face
+        20, 21, 22, 20, 22, 23  // Left face
+    ];
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+    cubeVertexIndexBuffer.itemSize = 1;
+    cubeVertexIndexBuffer.numItems = 36;
+}
+
+
+function makeDiamondVertecies(height, width, sides) {
+
+    var vertices = [
+        // Front face
+        -1.0, -1.0, 1.0,
+         1.0, -1.0, 1.0,
+         1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0, 1.0, -1.0,
+         1.0, 1.0, -1.0,
+         1.0, -1.0, -1.0,
+
+        // Top face
+        -1.0, 1.0, -1.0,
+        -1.0, 1.0, 1.0,
+         1.0, 1.0, 1.0,
+         1.0, 1.0, -1.0,
+
+        // Bottom face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0, 1.0,
+        -1.0, -1.0, 1.0,
+
+        // Right face
+         1.0, -1.0, -1.0,
+         1.0, 1.0, -1.0,
+         1.0, 1.0, 1.0,
+         1.0, -1.0, 1.0,
+
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0, 1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, 1.0, -1.0
+    ];
+
+    var unpackedColors = [];
+    for (var i in colors) {
+        var color = colors[i];
+        for (var j = 0; j < 4; j++) {
+            unpackedColors = unpackedColors.concat(color);
+        }
+    }
+
+    return (vertices);
+}
---
+++ WebGL-SampleWebApp/js/diamond.js
@@ -0,0 +1,94 @@
+﻿var diamondVertexPositionBuffer;
+var diamondVertexColorBuffer;
+var diamondVertexIndexBuffer;
+
+var point = { x: 0, y: 0, z: 0 };
+
+function Point() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+
+}
+
+var numSides = 7;
+var height = 2;
+var rad = 1;
+var dimention = 3;
+var colourDimention = 4;
+
+function numberOfItems(sides) {
+    var total = sides * 3 * 2;
+    return total;
+}
+
+function initBuffers() {
+    
+    diamondVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, diamondVertexPositionBuffer);
+    var vertices = makeDiamondVertecies(height, rad, numSides);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    diamondVertexPositionBuffer.itemSize = dimention;
+    diamondVertexPositionBuffer.numItems = numberOfItems(numSides);
+
+    diamondVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, diamondVertexColorBuffer);
+
+    var colour = [47/255, 198/255, 1.0, 1.0];
+    var colourArray = createColourPallet(colour);
+    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colourArray), gl.STATIC_DRAW);
+    diamondVertexColorBuffer.itemSize = colourDimention;
+    diamondVertexColorBuffer.numItems = numberOfItems(numSides);
+}
+
+function makeDiamondVertecies(height, radius, sides) {
+
+    var points = [];
+    var top = [];
+    var bottom = [];
+
+    for (var i = 0; i < sides; i++) {
+        top = drawSide(i, sides, radius, height); // top triangle
+        points.concat(top);
+        bottom = drawSide(i, sides, radius, -height); // bottom triangle
+        points.concat(bottom);
+    }
+
+    return (points);
+}
+
+function drawSide(sideNum, totalSides, radius, height) {
+    var triangleVertices = [];
+
+    var alpha = 2 * Math.PI / totalSides * sideNum;
+    var beta = 2 * Math.PI / totalSides * (sideNum + 1) % totalSides;
+
+    var point1 = createPoint(radius, alpha);
+    var point2 = createPoint(radius, beta);
+    var point3 = [0, height, 0];
+
+    triangleVertices.concat(point1);
+    triangleVertices.concat(point2);
+    triangleVertices.concat(point3);
+    
+    return triangleVertices;
+} 
+
+function createPoint(radius, theta) {
+    var point = new Point();
+
+    point.x = radius * Math.cos(theta);
+    point.y = 0;
+    point.z = radius * Math.sin(theta);
+
+    return [point.x, point.y, point.z];
+}
+
+function createColourPallet(colour) {
+    var colourPallet = [];
+    for (var i = 0; i < numSides; i++) {
+        colourPallet = colourPallet.concat(colour);
+    }
+    return colourPallet;
+}
